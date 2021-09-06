8b6277ea10836a2ffd38048e0b797b979aae0c21
Carlos Nunez <carlosonunez@noreply.github.com>
2021-09-06 02:02:25 -0500

    Welp we tried

--- lib/tesla_auth_bot/authentication/token.rb
+++ lib/tesla_auth_bot/authentication/token.rb
@@ -8,7 +8,22 @@ module TeslaAuthBot
     # Handles everything around retrieving access and secret tokens.
     module Token
       def self.begin_authenticating!(creds)
-        params = {
+        response = HTTP.execute!(uri: creds.authorize_url,
+                                 query: auth_params(creds))
+        page = Nokogiri::HTML(response.body)
+        puts "Headers: #{response.headers}"
+        session = {
+          session_cookie: response.headers['Set-Cookie']
+        }
+        %w[_csrf _phase _process transaction_id cancel].each do |attr_name|
+          attribute = page.at("input[@name=\"#{attr_name}\"]") or {}
+          session[attr_name.to_sym] = attribute['value'] unless attribute.nil?
+        end
+        session
+      end
+
+      def self.auth_params(creds)
+        {
           client_id: 'ownerapi',
           code_challenge: creds.challenge,
           code_challenge_method: 'S256',
@@ -17,25 +32,21 @@ module TeslaAuthBot
           scope: 'openid email offline_access',
           state: creds.state
         }
-        page = HTTP.get_or_raise!(creds.authorize_url, params)
-        session = {}
-        %w[_csrf _phase _process transaction_id cancel].each do |attr_name|
-          attribute = page.at("input[@name=\"#{attr_name}\"]") or {}
-          session[attr_name.to_sym] = if attribute.nil?
-                                        nil
-                                      else
-                                        attribute['value']
-                                      end
-        end
-        session
       end
     end
 
     # Methods for interacting with the Tesla authentication web pages.
     module HTTP
-      def self.get_or_raise!(uri, params = {})
-        page = HTTParty.get(uri, query: params)
-        Nokogiri::HTML(page)
+      def self.execute!(uri:,
+                        method: 'GET',
+                        headers: {},
+                        expected_code: 200,
+                        query: {})
+        resp = HTTParty.get(uri, method: method, headers: headers, query: query)
+        raise "Expected #{expected_code} but got #{resp.code}: #{resp.body}" \
+          unless resp.code == 200
+
+        resp
       rescue StandardError => e
         raise e
       end
--- spec/unit/authentication/token_spec.rb
+++ spec/unit/authentication/token_spec.rb
@@ -13,11 +13,19 @@ describe 'Given a library that produces access and refresh tokens' do
     allow_any_instance_of(Credentials).to receive(:state).and_return(@state)
   end
   example 'It retrieves session fields' do
-    fake_authorize_page = File.read('./spec/fixtures/step_1.html')
+    fake_authorize_page = OpenStruct.new({
+                                           code: 200,
+                                           headers: {
+                                             'Set-Cookie': 'fake-cookie'
+                                           }.transform_keys(&:to_s),
+                                           body: File.read('./spec/fixtures/step_1.html')
+                                         })
     creds = Credentials.new
     expect(HTTParty)
       .to receive(:get)
       .with('https://auth.tesla.com/oauth2/v3/authorize',
+            headers: {},
+            method: 'GET',
             query: {
               client_id: 'ownerapi',
               code_challenge: creds.challenge,
@@ -34,5 +42,41 @@ describe 'Given a library that produces access and refresh tokens' do
     expect(session[:_process]).to eq '1'
     expect(session[:transaction_id]).to eq 'fake-transaction-id'
     expect(session[:cancel]).to eq ''
+    expect(session[:session_cookie]).to eq 'fake-cookie'
+  end
+
+  example 'It retrieves an access code after retrieving session fields' do
+    fake_session = {
+      _csrf: 'fake-csrf-token',
+      _phase: 'authenticate',
+      _process: '1',
+      transaction_id: 'fake-transaction-id',
+      cancel: ''
+    }
+    fake_location = "https://auth.tesla.com/void/callback?code=fake-code&state=#{@state}&issuer=https%3A%2F%2Fauth.tesla.com%2Foauth2%2Fv3"
+    allow(Token).to receive(:begin_authenticating!).and_return(fake_session)
+    fake_code_page = OpenStruct.new({
+      code: 302,
+      headers: {
+        'Location': fake_location
+      },
+      body: File.read('./spec/fixtures/step_2.html')
+    }.transform_keys(&:to_s))
+    expect(HTTParty)
+      .to receive(:post)
+      .with('https://auth.tesla.com/oauth2/v3/authorize',
+            query: {
+              client_id: 'ownerapi',
+              code_challenge: creds.challenge,
+              code_challenge_method: 'S256',
+              redirect_uri: 'https://auth.tesla.com/void/callback',
+              response_type: 'code',
+              scope: 'openid email offline_access',
+              state: @state,
+              identity: 'fake@email.address',
+              credential: 'supersecret'
+            })
+      .and_return(fake_code_page)
+    expect(retrieve_access_code(creds, fake_session)).to eq 'fake-code'
   end
 end
